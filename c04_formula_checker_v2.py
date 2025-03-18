10f0f860b909c84dae7bc713ee048813f625139b
skibidimush <159740396+skibiditoil3t@users.noreply.github.com>
2025-03-19 10:02:47 +1300

    updated for triangles / trapeziums

--- c04_formula_checker_v2.py
+++ c04_formula_checker_v2.py
@@ -1,6 +1,50 @@
 import math
 
 
+def yes_no(question):
+    """check that users enter yes / y or no / n to a question"""
+
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("please enter yes (y) or no (n). \n")
+
+
+def string_checker(question, num_letters, valid_ans_list):
+    """"checks that users enter the full word
+    or the 'n' letter/s of a word from a list of valid responses"""
+
+    while True:
+        response = input(question).lower()
+
+        for item in valid_ans_list:
+            if response == item:
+                return item
+            elif response == item[:num_letters]:
+                return item
+
+            elif response == "t":
+                item = "triangle"
+                response = yes_no("Your chosen shape is triangle. Change to trapezium? ")
+                if response == "yes":
+                    item = "trapezium"
+                return item
+
+            elif response == "tri" or "tra":
+                if response == "tri":
+                    item = "triangle"
+                else:
+                    item = "trapezium"
+                return item
+
+        print(f"Please choose an option from {valid_ans_list}\n")
+
+
 def formula_check(question, shapes_list, exit_code=None):
     """asks user for their chosen shape then
     calculates the chosen shape by their formula"""
@@ -9,7 +53,8 @@ def formula_check(question, shapes_list, exit_code=None):
 
     error = "enter a valid shape"
 
-    response = input(question).lower()
+    response = string_checker(question, 1, shapes_list)
+    print("you chose", response)
 
     if response == exit_code:
         return response
@@ -27,7 +72,8 @@ def formula_check(question, shapes_list, exit_code=None):
     # and phrase question later on
     if response == "circle":
         radius = 0
-        area = 2 * math.pi * radius
+        perimeter = 2 * math.pi * radius
+        area = math.pi * (radius ** 2)
 
     # perhaps have 3 different types of triangles aka
     # right-angled, no 3rd side triangle (for perimeter question),
@@ -36,8 +82,8 @@ def formula_check(question, shapes_list, exit_code=None):
     elif response == "triangle":
         base = 0
         height = 0
-        triangle_side = 0
-        perimeter = base + height + triangle_side
+        side = 0
+        perimeter = base + height + side
         area = 1 / 2 * base * height
 
     elif response == "rectangle":
@@ -52,11 +98,11 @@ def formula_check(question, shapes_list, exit_code=None):
         area = 0 ** 2
 
     elif response == "trapezium":
-        trapezium_base = 0
+        base_2 = 0
         base = 0
         height = 0
         perimeter = 0
-        area = ((trapezium_base + base) / 2) * height
+        area = ((base_2 + base) / 2) * height
 
     return area, perimeter
 
@@ -67,5 +113,4 @@ shapes = ["circle", "triangle", "rectangle", "square", "trapezium"]
 
 while True:
     formula = formula_check("What shape are you solving? ", shapes)
-    print(str(formula))
     print()
