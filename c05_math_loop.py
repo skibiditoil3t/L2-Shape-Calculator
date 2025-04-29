19a976fb55b426427619843a6e4061371c6f2c6d
skibidimush <159740396+skibiditoil3t@users.noreply.github.com>
2025-04-30 11:01:51 +1200

    fixed 161-163 error

--- c05_math_loop.py
+++ c05_math_loop.py
@@ -95,6 +95,7 @@ def formula_check(response, exit_code):
 
     perimeter = 0
     area = 0
+    hypotenuse = 0
 
     # will replace values with random.randint()
     # and phrase question later on
@@ -107,9 +108,10 @@ def formula_check(response, exit_code):
     # 3 side triangles
 
     elif response == "triangle":
-        triangle_sides = number_checker("Number of unknown sides: ")
-        if triangle_sides == 1:
+        triangle_sides = number_checker("Right-Angled? ")
+        if triangle_sides == "yes":
             area = 1 / 2 * a * b
+            hypotenuse = math.sqrt(a**2 + b**2)
 
     elif response == "rectangle":
         area = ((a ** 2) * (b ** 2))
@@ -123,11 +125,13 @@ def formula_check(response, exit_code):
         area = ((a + b) / 2) * c
         perimeter = a + b + c + d
 
-    if perimeter or area <= 0:
+    elif perimeter or area <= 0:
         print(f"The perimeter / area must be non-negative or not equal to 0, "
               f"please check your values.\n")
 
-    return area, perimeter
+    print(perimeter)
+    print(area)
+    return area, perimeter, hypotenuse
 
 
 # initialise variables
@@ -141,6 +145,7 @@ shape_list = ["circle", "triangle", "rectangle", "square", "trapezium"]
 while True:
     # ask user for the shape
     shape = string_checker("What shape are you solving? ", 1, shape_list)
+    print(shape)
 
     # Explain it was something to do with for loop? Crazy, I know.
     for i in range(4):
@@ -149,22 +154,18 @@ while True:
         if length == "xxx":
             break
 
-        # for testing purposes, include in debugging slide
-        print("variables", variables)
         variables.append(length)
 
     n = len(variables)
-    print(n)
 
-    # if missing 4 sides, put in n variables as substitutes
-    # solves code list error
-    if len(variables) < 4:
-        for variables in range(4 - n):
+    # if not all 4 sides present, put in n variables as substitutes
+    if n < 4:
+        for i in range(4 - n):
             variables.append(0)
 
     a, b, c, d = variables
-    print(variables)
 
     # calculate the shape's area / perimeter
     formula = formula_check(shape, "xxx")
     print("you chose", shape, "\n")
+    print(formula)
